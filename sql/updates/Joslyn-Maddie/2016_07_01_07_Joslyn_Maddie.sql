4fd6a16a053dd4f35c63e3969bf3761401b3885f
Joslyn <sallpally@gmail.com>
2016-07-01 22:23:28 -0400

    Might need more work on the fire path.
    The flame pillars work now.

--- sql/updates/Joslyn-Maddie/2016_07_01_07_Joslyn_Maddie.sql
+++ sql/updates/Joslyn-Maddie/2016_07_01_07_Joslyn_Maddie.sql
@@ -14,6 +14,20 @@ UPDATE `creature_template` SET `modelid1` = '11686' WHERE `entry` = '56739' ;
 DELETE FROM `creature_template_addon` WHERE `entry` = '59626' ;
 INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('59626', '0', '0', '0', '1', '0', '114686');
 UPDATE `creature_template` SET `unit_flags` = '33555202' WHERE `entry` = '59626' ;
+DELETE FROM smart_scripts WHERE entryorguid=59626;
+INSERT INTO `smart_scripts` VALUES (59626, 0, 0, 0, 1, 0, 100, 1, 100, 100, 0, 0, 11, 114686, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Flame jet - Visual ground');
+INSERT INTO `smart_scripts` VALUES (59626, 0, 1, 0, 1, 0, 100, 0, 0, 0, 10000, 10000, 11, 114685, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Flame Jet - Fight off damage');
+INSERT INTO `smart_scripts` VALUES (59626, 0, 2, 0, 1, 0, 100, 0, 0, 0, 10000, 10000, 11, 114684, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Flare - Visual Effect flames out of combat');
+INSERT INTO `smart_scripts` VALUES (59626, 0, 3, 0, 0, 0, 100, 0, 0, 0, 10000, 10000, 11, 114685, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Flame Jet - fight Damage');
+INSERT INTO `smart_scripts` VALUES (59626, 0, 4, 0, 0, 0, 100, 0, 0, 0, 10000, 10000, 11, 114684, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Flare - Visual Effect fighting in flames');
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=59626;
+
+
+DELETE FROM `creature` WHERE `guid` = '940414' ;
+DELETE FROM `creature` WHERE `guid` = '940417' ;
+DELETE FROM `creature` WHERE `guid` = '940421' ;
+DELETE FROM `creature` WHERE `guid` = '940419' ;
+DELETE FROM `creature` WHERE `guid` = '940422' ;
 
 
 -- "Fix" quest "()" fix the phasing and spawns
